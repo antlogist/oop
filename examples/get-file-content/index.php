3065d483b165f270661d74e970b3fe4304082a14
antlogist <antlogist@gmail.com>
2021-05-29 10:38:05 +0300

    Get file content example added

---
+++ examples/get-file-content/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>File Class</title>
+</head>
+
+<body>
+  <div id="resultES5"></div>
+  <div id="resultES6"></div>
+  
+  <?php
+  // PHP
+  class File {
+    public $fileName;
+    public $content;
+
+    function displayContent() {
+      echo $this->content;
+    }
+    
+    function getContent() {
+      if (file_exists($this->fileName)) {
+        $this->content = file_get_contents($this->fileName);
+      }
+    }
+  }
+
+  $file = new File();
+  $file->fileName = "Test.txt";
+  $file->displayContent();
+  $file->getContent();
+  $file->displayContent();
+
+?>
+</body>
+
+<script>
+  
+  // ES5
+  const renderES5 = document.getElementById("resultES5");
+  
+  function FileES5(fileName, content) {
+    this.fileName = fileName;
+    this.content = content;
+    this.getContent = function() {
+      this.content = "<?php echo $file->displayContent(); ?>";
+    }
+    this.displayContent = function() {
+      return this.content;
+    }
+  }
+  
+  const fileES5 = new FileES5();
+  fileES5.getContent();
+  fileES5.displayContent();
+
+  renderES5.insertAdjacentHTML('beforeEnd', fileES5.content);
+
+  // ES6
+  const renderES6 = document.getElementById("resultES6");
+  
+  class FileES6 {
+    constructor(fileName, content) {
+      this.fileName = fileName;
+      this.content = content;
+    }
+    getContent() {
+      this.content = "<?php echo $file->displayContent(); ?>";
+    }
+    displayContent() {
+      return this.content;
+    }
+  }
+  
+  const fileES6 = new FileES6();
+  fileES6.getContent();
+  fileES6.displayContent();
+
+  renderES6.insertAdjacentHTML('beforeEnd', fileES6.content);  
+  
+</script>
+
+</html>
